01dfffa487a2550196816498b9311f2bb02eca8d
Will Bell <willbell@gmail.com>
2014-03-04 17:39:00 +0000

    files from rails new command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingDub::Application.config.secret_key_base = '0a123a4570870c243873887145a515cd62fed138aed0def3d4ff13c8d7630fe1c736b6d9a3b0f4a91eeddb539ea9f2f599654b5ee3e009c10f2c0bedcefc2d06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PingDub::Application.config.session_store :cookie_store, key: '_ping_dub_session'
